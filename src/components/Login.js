4b7540ff78c50bee12392987cbc48a1ebd3a4325
Ajith <ajit.v.billava@gmail.com>
2021-09-12 19:00:03 +0530

    added test credentials btn

--- src/components/Login.js
+++ src/components/Login.js
@@ -45,14 +45,20 @@ export const Login = () =>{
             <form className=" card block-card">
                 <div className="center sub-heading mr1">Login page</div>
                 <div className="vertical-card center">
-                    <input className="input curve" type="email" placeholder="Email" required 
+                    <input className="input curve" value={email} type="email" placeholder="Email" required 
                     onChange={(e) =>
                         dispatch({ type: "email", payload: e.target.value })
                       }></input>
-                    <input className="input curve" type="password" placeholder="Password" required
+                    <input className="input curve" value={password} type="password" placeholder="Password" required
                     onChange={(e) =>
                         dispatch({ type: "password", payload: e.target.value })
                       }></input>
+                    <button className="primary-btn btn" onClick={(e)=>
+                        {
+                            dispatch({type:"email",payload:"test@gmail.com"})
+                            dispatch({type:"password",payload:"123456"})
+                            e.preventDefault()
+                        }}>fill test credentials</button>
                     <button  className="secondary-btn md-btn btn" value="send" onClick={(e)=>
                     {       
                         if(!isAuthenticated){
